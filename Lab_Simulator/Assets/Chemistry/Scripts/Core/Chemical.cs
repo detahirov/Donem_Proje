8a41e67f605ec544cd821d16d322199aa79bf762
hkndeveloper <hk9916971omg@gmail.com>
2025-11-19 13:14:25 +0300

    hkn_002_missionsystem1

--- Lab_Simulator/Assets/Chemistry/Scripts/Core/Chemical.cs
+++ Lab_Simulator/Assets/Chemistry/Scripts/Core/Chemical.cs
@@ -7,7 +7,14 @@ public class Chemical : MonoBehaviour
     public SubstanceSO substance;   // hangi madde?
     public float amount = 0.05f;    // kg/oyunsal birim
     public bool consumedOnAdd = true;
-
+    void Start()
+    {
+        // Eðer bu madde kilitliyse kendini görünmez/yok yap
+        if (!UnlockSystem.IsSubstanceUnlocked(substance))
+        {
+            gameObject.SetActive(false);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         var cont = other.GetComponentInParent<Container>();
